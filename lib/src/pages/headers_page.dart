db20440595425b7ddd627e71510b5cea2bd0f131
Cristian Morales <cr15moresp@gmail.com>
2021-03-27 13:39:35 -0600

    Se preparó un cuadrado para ser animado

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:backgrounds_custom_painter/src/widgets/headers.dart';
+// import 'package:backgrounds_custom_painter/src/widgets/headers.dart';
+import 'package:backgrounds_custom_painter/src/pages/animaciones_page.dart';
 
 class HeadersPage extends StatelessWidget {
   @override
@@ -9,7 +10,8 @@ class HeadersPage extends StatelessWidget {
       // body: HeaderCuadrado(),
       // body: HeaderBordesRedondeados(),
       // body: HeaderTrianguloDerecho(),
-      body: HeaderWavesGradient(),
+      // body: HeaderWavesGradient(),
+      body: AnimacionesPage(),
     );
   }
 }
