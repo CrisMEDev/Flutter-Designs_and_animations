5237f3650d1f213fb1ce03308073acda1cd7c911
Cristian Morales <cr15moresp@gmail.com>
2021-03-30 19:49:53 -0600

    Se realizó el reto de mover un cuadrado en 4 puntos diferentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:backgrounds_custom_painter/src/retos/cuadrado_animado_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:backgrounds_custom_painter/src/pages/headers_page.dart';
+// import 'package:backgrounds_custom_painter/src/pages/headers_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños app',
-      home: HeadersPage(),
+      home: CuadradoAnimadoPage(),
     );
   }
 }
