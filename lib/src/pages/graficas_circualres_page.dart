1f486b30938788607857a21338ca81fe4f51e813
Cristian Morales <cr15moresp@gmail.com>
2021-04-01 23:13:44 -0600

    Se realizó un ejercició divertido con el radial progress

--- lib/src/pages/graficas_circualres_page.dart
+++ lib/src/pages/graficas_circualres_page.dart
@@ -23,20 +23,68 @@ class _GraficaCircularPageState extends State<GraficaCircularPage> {
           });
         }
       ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.20,
-          height: MediaQuery.of(context).size.width * 0.20,
-          // color: Colors.red,
-
-          child: RadialProgress(
-            porcentaje: porcentaje,
-            colorPrimario: Colors.green,
-            colorSecundario: Colors.brown,
-            grosorSecundario: 5.0,
-            grosorPrimario: 5.0,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomRadialProgress(
+                porcentaje: porcentaje,
+                colorPrimario: Colors.blue,
+                colorSecundario: Colors.grey,),
+              CustomRadialProgress(
+                porcentaje: porcentaje,
+                colorPrimario: Colors.grey,
+                colorSecundario: Colors.white38,),
+            ],
           ),
-        )
+          SizedBox( height: 15.0, ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomRadialProgress(
+                porcentaje: porcentaje,
+                colorPrimario: Colors.deepOrangeAccent,
+                colorSecundario: Colors.deepPurpleAccent,),
+              CustomRadialProgress(
+                porcentaje: porcentaje,
+                colorPrimario: Colors.black,
+                colorSecundario: Colors.cyan,),
+            ],
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class CustomRadialProgress extends StatelessWidget {
+
+  final Color colorPrimario;
+  final Color colorSecundario;
+
+  const CustomRadialProgress({
+    @required this.porcentaje,
+              this.colorPrimario,
+              this.colorSecundario
+  });
+
+  final double porcentaje;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.40,
+      height: MediaQuery.of(context).size.width * 0.40,
+      // color: Colors.red,
+
+      child: RadialProgress(
+        porcentaje: porcentaje,
+        colorPrimario: colorPrimario,
+        colorSecundario: colorSecundario,
+        grosorSecundario: 5.0,
+        grosorPrimario: 5.0,
       ),
     );
   }
