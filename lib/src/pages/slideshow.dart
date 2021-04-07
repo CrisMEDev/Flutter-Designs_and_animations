8917730cad7e4c83fd16148f6a8208f58d93b70b
Cristian Morales <cr15moresp@gmail.com>
2021-04-07 06:51:51 -0500

    Se creó el slideshow

--- lib/src/pages/slideshow.dart
+++ lib/src/pages/slideshow.dart
@@ -7,15 +7,51 @@ class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-  final String assetName = 'assets/svg/slide-1.svg';
-  final Widget svg = SvgPicture.asset(
-    assetName,
-    semanticsLabel: 'Logo'
-  );
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: _Slides(),
+        ),
+      ),
+    );
+  }
+}
 
-    return Scaffold(
-      body: Center(
-        child: svg,
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: PageView(
+        physics: ScrollPhysics(), // Elimina la animación azul presentada en android cuando no hay mas contenido
+        children: [
+          _Slide( assetSvg: 'assets/svg/slide-1.svg', ),
+
+          _Slide( assetSvg: 'assets/svg/slide-2.svg', ),
+
+          _Slide( assetSvg: 'assets/svg/slide-3.svg', )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String assetSvg;
+
+  const _Slide({@required this.assetSvg});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30.0),
+
+      child: SvgPicture.asset(
+        assetSvg,
+        semanticsLabel: 'Logo'
       ),
     );
   }
